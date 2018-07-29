fc1ec9c865ff588929e898c236b585ed3b92d239
LinuxLearnorg <41876740+LinuxLearnorg@users.noreply.github.com>
2018-07-30 00:44:53 +0430

    Create Bingwall1.sh

---
+++ Bingwall1.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
+
+mkdir -p $PICTURE_DIR
+
+urls=( $(curl -s http://www.bing.com | \
+    grep -Eo "url:'.*?'" | \
+    sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" | \
+    sed -e "s/\\\//g") )
+
+for p in ${urls[@]}; do
+    filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
+    if [ ! -f $PICTURE_DIR/$filename ]; then
+        echo "Downloading: $filename ..."
+        curl -Lo "$PICTURE_DIR/$filename" $p
+    else
+        echo "Skipping: $filename ..."
+    fi
+
+gsettings set org.gnome.desktop.background picture-uri "file://$PICTURE_DIR/$filename"
+
+done
